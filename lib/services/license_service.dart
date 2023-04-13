833bfc91c1b25b6cb2e5c232c4308fa232ee3dc2
John Peterson <nxh7@duck.com>
2023-04-13 14:02:16 +0330

    Add license service

---
+++ lib/services/license_service.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LicenseService {
+  Future<bool> validateServerSide(String licenseKey) async {
+    var url = Uri.parse("http://localhost/enc/");
+    Map<String, dynamic> body = {"license_key": licenseKey};
+    http.Response request = await http.post(url, body: body);
+    return (request.statusCode == 200);
+  }
+}
